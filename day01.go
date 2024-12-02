6af2005216e75619c9c9ab43efbbd44c223ac0d2
Henrik Sandberg <48967132+henrik-sandberg@users.noreply.github.com>
2024-12-02 11:26:38 +0100

    Day 1 and 2

--- day01.go
+++ day01.go
@@ -1,4 +1,29 @@
 package main
 
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
+
 func Day01(input []string) {
+	parsed := [][]int{{}, {}}
+	for _, line := range input {
+		ints := ToInts(strings.Fields(line))
+		parsed[0] = append(parsed[0], ints[0])
+		parsed[1] = append(parsed[1], ints[1])
+	}
+	sort.Sort(sort.IntSlice(parsed[0]))
+	sort.Sort(sort.IntSlice(parsed[1]))
+	part1 := 0
+	for i := range input {
+		part1 = part1 + abs(parsed[0][i]-parsed[1][i])
+	}
+	fmt.Println(part1)
+
+	part2 := 0
+	for _, v := range parsed[0] {
+		part2 = part2 + v*Count(parsed[1], v)
+	}
+	fmt.Println(part2)
 }
--- strings.go
+++ strings.go
@@ -1,9 +1,20 @@
 package main
 
+import "strconv"
+
 func Reverse(s string) string {
 	rns := []rune(s)
 	for i, j := 0, len(rns)-1; i < j; i, j = i+1, j-1 {
 		rns[i], rns[j] = rns[j], rns[i]
 	}
 	return string(rns)
+}
+
+func ToInts(input []string) []int {
+	ints := make([]int, len(input), len(input))
+	for i, s := range input {
+		value, _ := strconv.Atoi(s)
+		ints[i] = value
+	}
+	return ints
 }
