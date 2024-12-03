0081a201981e4675f3352f67a447ea4e1bce5064
Henrik Sandberg <48967132+henrik-sandberg@users.noreply.github.com>
2024-12-03 14:45:22 +0100

    Add day 3

--- day03.go
+++ day03.go
@@ -1,4 +1,31 @@
 package main
 
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
 func Day03(input []string) {
+	inp := strings.Join(input, "")
+	re := regexp.MustCompile(`mul\((\d+),(\d+)\)|(do|don't)\(\)`)
+	part1 := 0
+	part2 := 0
+	enabled := true
+	for _, match := range re.FindAllStringSubmatch(inp, -1) {
+		a, _ := strconv.Atoi(match[1])
+		b, _ := strconv.Atoi(match[2])
+		part1 += a * b
+		if match[3] == "do" {
+			enabled = true
+		} else if match[3] == "don't" {
+			enabled = false
+		}
+		if enabled {
+			part2 += a * b
+		}
+	}
+	fmt.Println(part1)
+	fmt.Println(part2)
 }
