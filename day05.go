5e71345404605a5ff5b2591b9c77aaa90dc94308
Henrik Sandberg <48967132+henrik-sandberg@users.noreply.github.com>
2024-12-08 12:29:21 +0100

    More polishing

--- day05.go
+++ day05.go
@@ -7,17 +7,28 @@ import (
 )
 
 func Day05(input []string) {
-	ind := IndexOf(input, "")
+	orderIndex := IndexOf(input, "")
 	rules := map[string][]string{}
-	for _, rule := range input[:ind] {
+	for _, rule := range input[:orderIndex] {
 		r := strings.Split(rule, "|")
 		rules[r[1]] = append(rules[r[1]], r[0])
 	}
+	orderSortFunc := func(a, b string) int {
+		if slices.Contains(rules[a], b) {
+			return 1
+		}
+		if slices.Contains(rules[b], a) {
+			return -1
+		}
+		return 0
+	}
 	part1 := 0
 	part2 := 0
-	for _, order := range input[ind+1:] {
+	for _, order := range input[orderIndex+1:] {
 		ord := strings.Split(order, ",")
-		sorted := ordersSorted(ord, rules)
+		sorted := make([]string, len(ord))
+		copy(sorted, ord)
+		slices.SortStableFunc(sorted, orderSortFunc)
 		pageValue := ToInts(sorted)[len(sorted)/2]
 		if slices.Equal(ord, sorted) {
 			part1 += pageValue
@@ -27,19 +38,4 @@ func Day05(input []string) {
 	}
 	fmt.Println(part1)
 	fmt.Println(part2)
-}
-
-func ordersSorted(order []string, rules map[string][]string) []string {
-	s := make([]string, len(order))
-	copy(s, order)
-	slices.SortStableFunc(s, func(a, b string) int {
-		if slices.Contains(rules[a], b) {
-			return 1
-		}
-		if slices.Contains(rules[b], a) {
-			return -1
-		}
-		return 0
-	})
-	return s
 }
