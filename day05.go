19f2f4222b2acada869f40d217e9b320e27f3d67
Henrik Sandberg <48967132+henrik-sandberg@users.noreply.github.com>
2024-12-08 12:03:14 +0100

    Improve day 5 with sorting

--- day05.go
+++ day05.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"slices"
 	"strings"
 )
 
@@ -16,45 +17,29 @@ func Day05(input []string) {
 	part2 := 0
 	for _, order := range input[ind+1:] {
 		ord := strings.Split(order, ",")
-		if isValidOrder(ord, rules) {
-			part1 += ToInts(ord)[len(ord)/2]
+		sorted := ordersSorted(ord, rules)
+		pageValue := ToInts(sorted)[len(sorted)/2]
+		if slices.Equal(ord, sorted) {
+			part1 += pageValue
 		} else {
-			fixed := fixInvalidOrder(ord, rules)
-			part2 += ToInts(fixed)[len(ord)/2]
+			part2 += pageValue
 		}
 	}
 	fmt.Println(part1)
 	fmt.Println(part2)
 }
 
-func isValidOrder(order []string, rules map[string][]string) bool {
-	printed := map[string]bool{}
-	for _, page := range order {
-		for _, needed := range rules[page] {
-			if !printed[needed] && Contains(order, needed) {
-				return false
-			}
+func ordersSorted(order []string, rules map[string][]string) []string {
+	s := make([]string, len(order))
+	copy(s, order)
+	slices.SortStableFunc(s, func(a, b string) int {
+		if slices.Contains(rules[a], b) {
+			return 1
 		}
-		printed[page] = true
-	}
-	return true
-}
-
-func fixInvalidOrder(order []string, rules map[string][]string) []string {
-	ret := []string{}
-	for len(ret) < len(order) {
-	findPage:
-		for _, page := range order {
-			if Contains(ret, page) {
-				continue
-			}
-			for _, needed := range rules[page] {
-				if !Contains(ret, needed) && Contains(order, needed) {
-					continue findPage
-				}
-			}
-			ret = append(ret, page)
+		if slices.Contains(rules[b], a) {
+			return -1
 		}
-	}
-	return ret
+		return 0
+	})
+	return s
 }
