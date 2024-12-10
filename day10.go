4b532f49c80d1021af9ddcb52a720ab437f44c09
Henrik Sandberg <48967132+henrik-sandberg@users.noreply.github.com>
2024-12-10 15:52:34 +0100

    Add day 10

--- day10.go
+++ day10.go
@@ -1,4 +1,35 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 func Day10(input []string) {
+	grid := Map{strings.Join(input, ""), len(input[0])}.ToComplexGrid()
+	solve := func(part1 bool) (res int) {
+		for start, val := range grid {
+			if val == '0' {
+				seen := map[complex64]bool{}
+				queue := []complex64{start}
+				for len(queue) > 0 {
+					p := queue[0]
+					queue = queue[1:]
+					for _, dir := range []complex64{1, -1, -1i, 1i} {
+						if next := p + dir; grid[p]+1 == grid[next] && !seen[next] {
+							seen[next] = part1
+							if grid[next] == '9' {
+								res++
+							} else {
+								queue = append(queue, next)
+							}
+						}
+					}
+				}
+			}
+		}
+		return
+	}
+	fmt.Println(solve(true))
+	fmt.Println(solve(false))
 }
