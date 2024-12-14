8f1adab0141b061a76fa82aa1027c7be1bae494c
Henrik Sandberg <48967132+henrik-sandberg@users.noreply.github.com>
2024-12-14 14:41:28 +0100

    Add day 14

--- day14.go
+++ day14.go
@@ -1,4 +1,56 @@
 package main
 
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
 func Day14(input []string) {
+	re := regexp.MustCompile(`-?\d+`)
+	bots := make([][]int, len(input))
+	for i, line := range input {
+		bots[i] = ToInts(re.FindAllString(line, -1))
+	}
+	width := 101
+	height := 103
+	move := func(bot []int, rounds int) (int, int) {
+		x := positiveMod(bot[0]+bot[2]*rounds, width)
+		y := positiveMod(bot[1]+bot[3]*rounds, height)
+		return x, y
+	}
+	quadrants := []int{0, 0, 0, 0}
+	for _, bot := range bots {
+		if x, y := move(bot, 100); x != width/2 && y != height/2 {
+			quadrants[2*x/width+2*(2*y/height)]++
+		}
+	}
+	fmt.Println(multiply(quadrants...))
+	for n := 1; ; n++ {
+		X, Y := make([]int, len(bots)), make([]int, len(bots))
+		for i, bot := range bots {
+			X[i], Y[i] = move(bot, n)
+		}
+		if sigx, sigy := stddev(X), stddev(Y); sigx+sigy < 40 {
+			fmt.Println(n)
+			break
+		}
+	}
+}
+
+func printChristmasTree(botX, botY []int, width, height int) {
+	output := make([][]string, height)
+	for h := 0; h < height; h++ {
+		row := make([]string, width)
+		for w := 0; w < width; w++ {
+			row[w] = "."
+		}
+		output[h] = row
+	}
+	for i := range botX {
+		output[botY[i]][botX[i]] = "x"
+	}
+	for _, line := range output {
+		fmt.Println(strings.Join(line, ""))
+	}
 }
--- numeric.go
+++ numeric.go
@@ -1,5 +1,7 @@
 package main
 
+import "math"
+
 func abs(i int) int {
 	if i < 0 {
 		return -i
@@ -44,7 +46,7 @@ func multiply(n ...int) int {
 
 func gcd(a, b int) int {
 	for b != 0 {
-              a, b = b, a % b
+		a, b = b, a%b
 	}
 	return a
 }
@@ -55,7 +57,26 @@ func lcm(n ...int) int {
 	}
 	res := n[0] * (n[1] / gcd(n[0], n[1]))
 	for _, v := range n[2:] {
-              res = lcm(res, v)
+		res = lcm(res, v)
 	}
 	return res
+}
+
+func stddev(arr []int) float64 {
+	sum := 0
+	for _, v := range arr {
+		sum += v
+	}
+	mean := float64(sum) / float64(len(arr))
+	variance := 0.0
+	for _, v := range arr {
+		diff := float64(v) - mean
+		variance += diff * diff
+	}
+	variance /= float64(len(arr))
+	return math.Sqrt(variance)
+}
+
+func positiveMod(i, mod int) int {
+	return (i%mod + mod) % mod
 }
