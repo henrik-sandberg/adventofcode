bfa132c406848a3f2f96c5b6820bf141c8d5fd09
Henrik Sandberg <48967132+henrik-sandberg@users.noreply.github.com>
2024-12-15 17:16:57 +0100

    Consolidate solution

--- day15.go
+++ day15.go
@@ -7,40 +7,33 @@ import (
 )
 
 func Day15(input []string) {
-	directions := map[rune]complex64{'<': -1, '^': -1i, '>': 1, 'v': 1i}
 	splitindex := slices.Index(input, "")
-
-	/*
-		for i := 0; i < splitindex; i++ {
-			input[i] = strings.ReplaceAll(input[i], "#", "##")
-			input[i] = strings.ReplaceAll(input[i], "O", "[]")
-			input[i] = strings.ReplaceAll(input[i], ".", "..")
-			input[i] = strings.ReplaceAll(input[i], "@", "@.")
-		}
-	*/
-	flatmap := Map{strings.Join(input[:splitindex], ""), len(input[0])}
-	m := flatmap.ToComplexGrid()
-	bot := flatmap.GetPointComplex(strings.Index(flatmap.cells, "@"))
-	m[bot] = '.'
-	printmap := func() {
-		for r := 0; r < flatmap.Height(); r++ {
-			sb := strings.Builder{}
-			for c := 0; c < flatmap.Width(); c++ {
-				cn := complex(float32(c), float32(r))
-				if cn == bot {
-					sb.WriteRune('@')
-				} else {
-					sb.WriteRune(m[cn])
-				}
-			}
-			// fmt.Println(sb.String())
-		}
+	moves := strings.Join(input[splitindex:], "")
+	setup := func() (map[complex64]rune, complex64) {
+		inpmap := Map{strings.Join(input[:splitindex], ""), len(input[0])}
+		bot := inpmap.GetPointComplex(strings.Index(inpmap.cells, "@"))
+		m := inpmap.ToComplexGrid()
+		m[bot] = '.'
+		return m, bot
 	}
-	printmap()
+
+	m, bot := setup()
+	fmt.Println(day15_sovler(m, bot, moves))
+	for i := 0; i < splitindex; i++ {
+		input[i] = strings.ReplaceAll(input[i], "#", "##")
+		input[i] = strings.ReplaceAll(input[i], "O", "[]")
+		input[i] = strings.ReplaceAll(input[i], ".", "..")
+		input[i] = strings.ReplaceAll(input[i], "@", "@.")
+	}
+	m, bot = setup()
+	fmt.Println(day15_sovler(m, bot, moves))
+}
+
+func day15_sovler(m map[complex64]rune, bot complex64, moves string) int {
+	directions := map[rune]complex64{'<': -1, '^': -1i, '>': 1, 'v': 1i}
 moveloop:
-	for _, move := range strings.Join(input[splitindex:], "")[:] {
+	for _, move := range moves {
 		dir := directions[move]
-		steps := 1
 		pointer := bot + dir
 		if m[pointer] == '#' {
 			continue
@@ -49,92 +42,51 @@ moveloop:
 			bot += dir
 			continue
 		}
-
+		box := pointer
+		boxes := []complex64{box}
 		if imag(dir) != 0 {
-			box := pointer
-			boxes := []complex64{box}
-
 			switch m[pointer] {
 			case '[':
 				boxes = append(boxes, box+1)
 			case ']':
 				boxes = append(boxes, box-1)
 			}
-			traversed := []complex64{}
-			for len(boxes) > 0 {
-				box, boxes = boxes[0], boxes[1:]
-				next := box + dir
-				if !slices.Contains(traversed, box) {
-					traversed = append(traversed, box)
-				}
-				switch m[next] {
-				case '#':
-					continue moveloop
-				case '[':
-					boxes = append(boxes, next, next+1)
-				case ']':
-					boxes = append(boxes, next, next-1)
-				case 'O':
-					boxes = append(boxes, next)
-				}
-				if v := abs(int(real(box)) - int(real(bot))); v > steps {
-					steps = v
-				}
+		}
+		seen := []complex64{}
+		for len(boxes) > 0 {
+			box, boxes = boxes[0], boxes[1:]
+			if !slices.Contains(seen, box) {
+				seen = append(seen, box)
 			}
-			slices.Reverse(traversed)
-			for _, box := range traversed {
-				m[box], m[box+dir] = m[box+dir], m[box]
+			next := box + dir
+			switch m[next] {
+			case '#':
+				continue moveloop
+			case '[':
+				boxes = append(boxes, next)
+				if imag(dir) != 0 {
+					boxes = append(boxes, next+1)
+				}
+			case ']':
+				boxes = append(boxes, next)
+				if imag(dir) != 0 {
+					boxes = append(boxes, next-1)
+				}
+			case 'O':
+				boxes = append(boxes, next)
 			}
-			bot += dir
-			continue
 		}
-
-		for m[pointer] != '#' && m[pointer] != '.' {
-			pointer += dir
-			steps++
-		}
-		if m[pointer] == '#' {
-			continue
-		}
-		for i := 0; i < steps; i++ {
-			m[pointer] = m[pointer-dir]
-			pointer -= dir
+		slices.Reverse(seen)
+		for _, box := range seen {
+			m[box], m[box+dir] = m[box+dir], m[box]
 		}
 		bot += dir
 	}
-	part2 := 0
+	res := 0
 	for k, v := range m {
-		if v == 'O' {
-			part2 += 100*int(imag(k)) + int(real(k))
+		if v == 'O' || v == '[' {
+			res += 100*int(imag(k)) + int(real(k))
 		}
 	}
-	fmt.Println(part2)
-
-	// PART 1
-
-	for _, move := range strings.Join(input[splitindex:], "")[:0] {
-		dir := directions[move]
-		// Find next empty space
-		steps := 1
-		pointer := bot + dir
-		for m[pointer] != '#' && m[pointer] != '.' {
-			pointer += dir
-			steps++
-		}
-		if m[pointer] == '#' {
-			continue
-		}
-		for i := 0; i < steps; i++ {
-			m[pointer] = m[pointer-dir]
-			pointer -= dir
-		}
-		bot += dir
-	}
-	part1 := 0
-	for k, v := range m {
-		if v == 'O' {
-			part1 += 100*int(imag(k)) + int(real(k))
-		}
-	}
-	fmt.Println(part1)
+	return res
 }
