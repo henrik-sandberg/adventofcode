81bcfac986d392f7263470d55d27cdc3a9109c3e
Henrik Sandberg <48967132+henrik-sandberg@users.noreply.github.com>
2024-12-21 13:15:55 +0100

    Add day 19

--- day19.go
+++ day19.go
@@ -1,4 +1,42 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 func Day19(input []string) {
+	available := strings.Split(input[0], ", ")
+	cache := map[string]int{}
+	var search func(string, []string) int
+	search = func(s string, work []string) int {
+		if len(s) == 0 {
+			return 1
+		}
+		if res, ok := cache[s]; ok {
+			return res
+		}
+		found := 0
+		for _, a := range available {
+			if strings.HasPrefix(s, a) {
+				work = append(work, a)
+				found += search(s[len(a):], work)
+				work = work[:len(work)-1]
+			}
+		}
+		cache[s] = found
+		return found
+	}
+	part1 := 0
+	part2 := 0
+	for _, design := range input[2:] {
+		found := search(design, []string{})
+		if found > 0 {
+			part1 += 1
+		}
+		part2 += found
+
+	}
+	fmt.Println(part1)
+	fmt.Println(part2)
 }
