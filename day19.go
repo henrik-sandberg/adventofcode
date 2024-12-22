420b1aeb247e1a3afe053cabd85bf30e5c0bac05
Henrik Sandberg <48967132+henrik-sandberg@users.noreply.github.com>
2024-12-22 22:17:46 +0100

    Named return variable

--- day19.go
+++ day19.go
@@ -9,23 +9,22 @@ func Day19(input []string) {
 	available := strings.Split(input[0], ", ")
 	cache := map[string]int{}
 	var search func(string, []string) int
-	search = func(s string, work []string) int {
+	search = func(s string, work []string) (res int) {
 		if len(s) == 0 {
 			return 1
 		}
-		if res, ok := cache[s]; ok {
-			return res
+		if v, ok := cache[s]; ok {
+			return v
 		}
-		found := 0
 		for _, a := range available {
 			if strings.HasPrefix(s, a) {
 				work = append(work, a)
-				found += search(s[len(a):], work)
+				res += search(s[len(a):], work)
 				work = work[:len(work)-1]
 			}
 		}
-		cache[s] = found
-		return found
+		cache[s] = res
+		return res
 	}
 	part1 := 0
 	part2 := 0
