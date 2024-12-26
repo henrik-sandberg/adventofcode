e7d5ff8adef8b72237bc1265603968542e610a75
Henrik Sandberg <48967132+henrik-sandberg@users.noreply.github.com>
2024-12-26 22:06:26 +0100

    Day 24 - part 2

--- day24.go
+++ day24.go
@@ -15,99 +15,69 @@ func Day24(input []string) {
 		tmp := strings.Split(register, ": ")
 		registers[tmp[0]], _ = strconv.Atoi(tmp[1])
 	}
-	gates := map[string][]string{}
+	type gate struct {
+		a, b string
+		op   string
+	}
+	inputPrefix := func(g gate, prefix string) bool {
+		return strings.HasPrefix(g.a, prefix) || strings.HasPrefix(g.b, prefix)
+	}
+	gates := map[string]gate{}
 	for _, calculation := range input[inputIndex+1:] {
 		tmp := strings.Fields(calculation)
-		gates[tmp[4]] = tmp[:3]
-	}
-	operands := map[string]func(int, int) int{
-		"AND": func(a, b int) int {
-			return a & b
-		},
-		"XOR": func(a, b int) int {
-			return a ^ b
-		},
-		"OR": func(a, b int) int {
-			return a | b
-		},
+		gates[tmp[4]] = gate{tmp[0], tmp[2], tmp[1]}
 	}
 	var loadValue func(string) int
 	loadValue = func(reg string) int {
-		if v, ok := registers[reg]; ok {
-			return v
+		if val, ok := registers[reg]; ok {
+			return val
 		}
 		gate := gates[reg]
-		v := operands[gate[1]](loadValue(gate[0]), loadValue(gate[2]))
-		registers[reg] = v
-		return v
+		lh, rh := loadValue(gate.a), loadValue(gate.b)
+		switch gate.op {
+		case "AND":
+			return lh & rh
+		case "XOR":
+			return lh ^ rh
+		case "OR":
+			return lh | rh
+		default:
+			panic("Unknown operand: " + gate.op)
+		}
 	}
-	Z := []string{}
+	part1 := 0
 	for reg := range maps.Keys(gates) {
 		if reg[0] == 'z' {
-			Z = append(Z, reg)
+			tmp, _ := strconv.Atoi(reg[1:])
+			part1 |= loadValue(reg) << tmp
 		}
-	}
-	slices.Sort(Z)
-	part1 := 0
-	for _, reg := range Z {
-		tmp, _ := strconv.Atoi(reg[1:])
-		part1 |= loadValue(reg) << tmp
 	}
 	fmt.Println(part1)
-	sumRegistry := func(prefix byte) (res int) {
-		for reg := range maps.Keys(registers) {
-			if reg[0] == prefix {
-				tmp, _ := strconv.Atoi(reg[1:])
-				res |= loadValue(reg) << tmp
-			}
+	bad := map[string]bool{}
+	for res, g := range gates {
+		if g.op != "XOR" && strings.HasPrefix(res, "z") && res != "z45" {
+			bad[res] = true
 		}
-		return
-	}
-	fmt.Println(sumRegistry('x'))
-
-	/*
-		sumRegistry := func(prefix byte) (res int) {
-			for reg := range maps.Keys(registers) {
-				if reg[0] == prefix {
-					tmp, _ := strconv.Atoi(reg[1:])
-					res |= loadValue(reg) << tmp
+		if g.op == "XOR" &&
+			!strings.HasPrefix(res, "z") &&
+			!inputPrefix(g, "x") &&
+			!inputPrefix(g, "y") {
+			bad[res] = true
+		}
+		if g.op == "AND" && !inputPrefix(g, "x00") {
+			for _, subg := range gates {
+				if subg.op != "OR" && inputPrefix(subg, res) {
+					bad[res] = true
 				}
 			}
-			return
 		}
-		expected := sumRegistry('x') & sumRegistry('y')
-		fmt.Println("Expected:", expected)
-		zRegisters := []string{}
-		for reg := range maps.Keys(gates) {
-			if reg[0] == 'z' {
-				zRegisters = append(zRegisters, reg)
+		if g.op == "XOR" {
+			for _, subg := range gates {
+				if subg.op == "OR" && inputPrefix(subg, res) {
+					bad[res] = true
+				}
 			}
 		}
-		keys := slices.Collect(maps.Keys(gates))
-		fmt.Println("z registers:", zRegisters)
-		for perm := range Permutations(keys, 8) {
-			registers = map[string]int{}
-			for _, register := range input[:inputIndex] {
-				tmp := strings.Split(register, ": ")
-				registers[tmp[0]], _ = strconv.Atoi(tmp[1])
-			}
-			gates[perm[0]], gates[perm[1]] = gates[perm[1]], gates[perm[0]]
-			gates[perm[2]], gates[perm[3]] = gates[perm[3]], gates[perm[2]]
-			gates[perm[4]], gates[perm[5]] = gates[perm[5]], gates[perm[4]]
-			gates[perm[6]], gates[perm[7]] = gates[perm[7]], gates[perm[6]]
-			res := 0
-			for _, reg := range zRegisters {
-				tmp, _ := strconv.Atoi(reg[1:])
-				res |= loadValue(reg) << tmp
-			}
-			if res == expected {
-				fmt.Println(perm, res)
-				panic("found")
-			}
-			gates[perm[0]], gates[perm[1]] = gates[perm[1]], gates[perm[0]]
-			gates[perm[2]], gates[perm[3]] = gates[perm[3]], gates[perm[2]]
-			gates[perm[4]], gates[perm[5]] = gates[perm[5]], gates[perm[4]]
-			gates[perm[6]], gates[perm[7]] = gates[perm[7]], gates[perm[6]]
-		}
-	*/
+	}
+	fmt.Println(strings.Join(slices.Sorted(maps.Keys(bad)), ","))
 }
