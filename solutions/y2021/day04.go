3c14f7c4007fe3d520daf22426e3f3b3082cd525
Henrik Sandberg <48967132+henrik-sandberg@users.noreply.github.com>
2025-11-30 13:39:36 +0100

    2021 day 4

--- solutions/y2021/day04.go
+++ solutions/y2021/day04.go
@@ -4,12 +4,15 @@ import (
 	"adventofcode/solutions/shared"
 	"math"
 	"slices"
-	"strconv"
 	"strings"
 )
 
 func Day04(input []string) (solution shared.Solution[int, int]) {
-	isSolved := func(b []string) bool {
+	type board struct {
+		values []int
+		marks  []bool
+	}
+	isSolved := func(b board) bool {
 		for _, winningSquares := range [][]int{
 			{0, 1, 2, 3, 4},
 			{5, 6, 7, 8, 9},
@@ -24,7 +27,7 @@ func Day04(input []string) (solution shared.Solution[int, int]) {
 		} {
 			solved := true
 			for _, idx := range winningSquares {
-				if b[idx] != "x" {
+				if !b.marks[idx] {
 					solved = false
 					break
 				}
@@ -35,28 +38,36 @@ func Day04(input []string) (solution shared.Solution[int, int]) {
 		}
 		return false
 	}
-	moves := strings.Split(input[0], ",")
+	moves := shared.IntSlice(strings.Split(input[0], ","))
 	first := math.MaxInt
 	second := 0
-	for i := 2; i < len(input); i += 6 {
-		board := strings.Fields(strings.Join(input[i:i+5], " "))
-		for used, m := range moves {
-			ind := slices.Index(board, m)
-			if ind == -1 {
+	for row := 2; row < len(input); row += 6 {
+		b := board{
+			values: shared.IntSlice(strings.Fields(strings.Join(input[row:row+5], " "))),
+			marks:  make([]bool, 25),
+		}
+		for used, move := range moves {
+			idx := slices.Index(b.values, move)
+			if idx == -1 {
 				continue
 			}
-			board[ind] = "x"
-			if !isSolved(board) {
+			b.marks[idx] = true
+			if !isSolved(b) {
 				continue
 			}
-			last, _ := strconv.Atoi(m)
+			unmarked := 0
+			for i := range 25 {
+				if !b.marks[i] {
+					unmarked += b.values[i]
+				}
+			}
 			if used < first {
 				first = used
-				solution.Part1 = shared.Sum(shared.IntSlice(board)...) * last
+				solution.Part1 = unmarked * move
 			}
 			if used > second {
 				second = used
-				solution.Part2 = shared.Sum(shared.IntSlice(board)...) * last
+				solution.Part2 = unmarked * move
 			}
 			break
 		}
