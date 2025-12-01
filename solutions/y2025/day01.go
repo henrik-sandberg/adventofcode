1043f3c704aada934b52429340116384f5e56a37
Henrik Sandberg <48967132+henrik-sandberg@users.noreply.github.com>
2025-12-01 19:45:45 +0100

    solution 2025 day 1

--- solutions/y2025/day01.go
+++ solutions/y2025/day01.go
@@ -1,9 +1,35 @@
 package y2025
 
 import (
+	"strconv"
+
 	"adventofcode/solutions/shared"
 )
 
 func Day01(input []string) (solution shared.Solution[int, int]) {
+	current := 50
+	for _, step := range input {
+		val, _ := strconv.Atoi(step[1:])
+
+		solution.Part2 += val / 100
+		rem := val % 100
+
+		if step[0] == 'L' {
+			if current > 0 && rem >= current {
+				solution.Part2++
+			}
+			current -= rem
+		} else {
+			if current+rem >= 100 {
+				solution.Part2++
+			}
+			current += rem
+		}
+
+		current = shared.PositiveMod(current, 100)
+		if current == 0 {
+			solution.Part1++
+		}
+	}
 	return
 }
--- solutions/y2025/day01_test.go
+++ solutions/y2025/day01_test.go
@@ -6,17 +6,26 @@ import (
 )
 
 func TestDay01(t *testing.T) {
-	sampleInput := ""
+	sampleInput := `L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82`
 
 	input := strings.Split(sampleInput, "\n")
 
 	res := Day01(input)
 
-	if expected := 0; expected != res.Part1 {
+	if expected := 3; expected != res.Part1 {
 		t.Errorf("part1: expected %d, got %d", expected, res.Part1)
 	}
 
-	if expected := 0; expected != res.Part2 {
+	if expected := 6; expected != res.Part2 {
 		t.Errorf("part2: expected %d, got %d", expected, res.Part2)
 	}
 }
