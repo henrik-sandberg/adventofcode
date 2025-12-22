56210dcbbc6c8345d47e93c147fdaaf080a139d0
Henrik Sandberg <48967132+henrik-sandberg@users.noreply.github.com>
2025-12-22 22:30:57 +0100

    solution 2025 day 12

--- solutions/y2025/day12.go
+++ solutions/y2025/day12.go
@@ -2,8 +2,26 @@ package y2025
 
 import (
 	"adventofcode/solutions/shared"
+	"strings"
 )
 
 func Day12(input []string) (solution shared.Solution[int, int]) {
+	var pixelCount []int
+	idx := 0
+	for ; !strings.ContainsRune(input[idx], 'x'); idx += 5 {
+		s := strings.Join(input[idx+1:idx+4], "")
+		pixelCount = append(pixelCount, strings.Count(s, "#"))
+	}
+	for ; idx < len(input); idx++ {
+		fs := strings.Fields(input[idx])
+		capacity := shared.Product(shared.IntSlice(strings.Split(strings.Trim(fs[0], ":"), "x"))...)
+		pixels := 0
+		for i, v := range shared.IntSlice(fs[1:]) {
+			pixels += pixelCount[i] * v
+		}
+		if pixels <= capacity {
+			solution.Part1 += 1
+		}
+	}
 	return
 }
