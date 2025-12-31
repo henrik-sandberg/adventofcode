a96a72c31612dbc0c341bd82748fc9910aa6cfd5
Henrik Sandberg <48967132+henrik-sandberg@users.noreply.github.com>
2025-12-31 10:11:29 +0100

    refactor: find solution at sensor edges

--- solutions/y2022/day15.go
+++ solutions/y2022/day15.go
@@ -3,8 +3,8 @@ package y2022
 import (
 	"adventofcode/solutions/shared"
 	"maps"
+	"math"
 	"regexp"
-	"slices"
 	"strconv"
 )
 
@@ -13,64 +13,70 @@ func Day15(input []string) shared.Solution[int, int] {
 }
 
 func day15solve(input []string, row int) (solution shared.Solution[int, int]) {
-	type Range struct {
-		from, to int
-	}
 	type Point struct {
 		x, y int
 	}
+	manhattan := func(a, b Point) int {
+		return shared.Abs(a.x-b.x) + shared.Abs(a.y-b.y)
+	}
+	re := regexp.MustCompile(`[xy]=(-?\d+)`)
 	parse := func(match [][]string, i int) Point {
 		x, _ := strconv.Atoi(match[i][1])
 		y, _ := strconv.Atoi(match[i+1][1])
 		return Point{x: x, y: y}
 	}
-	re := regexp.MustCompile(`[xy]=(-?\d+)`)
-	sensors := make(map[Point]Point, len(input))
-	beaconsOnTargetRow := make(map[Point]struct{})
+	radius := make(map[Point]int)
+	beacons := make(map[Point]struct{})
 	for _, line := range input {
 		match := re.FindAllStringSubmatch(line, 4)
 		sensor := parse(match, 0)
 		beacon := parse(match, 2)
-		sensors[sensor] = beacon
-		beaconsOnTargetRow[beacon] = struct{}{}
+		beacons[beacon] = struct{}{}
+		radius[sensor] = manhattan(sensor, beacon)
 	}
-	maps.DeleteFunc(beaconsOnTargetRow, func(p Point, _ struct{}) bool {
+	xmin, xmax := math.MaxInt, math.MinInt
+	for p, dist := range radius {
+		remaining := dist - shared.Abs(p.y-row)
+		xmin = min(xmin, p.x-remaining)
+		xmax = max(xmax, p.x+remaining)
+	}
+	maps.DeleteFunc(beacons, func(p Point, _ struct{}) bool {
 		return p.y != row
 	})
-	coveredPoints := func(row int) []Range {
-		var ranges []Range
-		for sensor, beacon := range sensors {
-			dist := shared.Abs(sensor.x-beacon.x) + shared.Abs(sensor.y-beacon.y)
-			perpendicularToY := shared.Abs(sensor.y - row)
-			if dist > perpendicularToY {
-				remaining := dist - perpendicularToY
-				ranges = append(ranges, Range{sensor.x - remaining, sensor.x + remaining})
+	solution.Part1 = xmax - xmin + 1 - len(beacons)
+
+	findNonCoveredPoint := func() Point {
+		acoeffs := make(map[int]struct{})
+		bcoeffs := make(map[int]struct{})
+		for p, r := range radius {
+			acoeffs[p.y-p.x+r+1] = struct{}{}
+			acoeffs[p.y-p.x-r-1] = struct{}{}
+			bcoeffs[p.x+p.y+r+1] = struct{}{}
+			bcoeffs[p.x+p.y-r-1] = struct{}{}
+		}
+		bound := 2 * row
+		for a := range acoeffs {
+			for b := range bcoeffs {
+				p := Point{x: (b - a) >> 1, y: (a + b) >> 1}
+				if p.x < 0 || p.x > bound ||
+					p.y < 0 || p.y > bound {
+					continue
+				}
+				valid := true
+				for s, dist := range radius {
+					if manhattan(s, p) <= dist {
+						valid = false
+						break
+					}
+				}
+				if valid {
+					return p
+				}
 			}
 		}
-		slices.SortFunc(ranges, func(a, b Range) int {
-			return a.from - b.from
-		})
-		merged := []Range{ranges[0]}
-		for _, r := range ranges[1:] {
-			last := &merged[len(merged)-1]
-			if r.from > last.to+1 {
-				merged = append(merged, r)
-			} else if r.to > last.to {
-				last.to = r.to
-			}
-		}
-		return merged
+		panic("could not find non/covered point")
 	}
-
-	covered := coveredPoints(row)[0]
-	solution.Part1 = covered.to - covered.from + 1 - len(beaconsOnTargetRow)
-
-	for i := range 2*row + 1 {
-		covered := coveredPoints(i)
-		if len(covered) > 1 {
-			solution.Part2 = (covered[0].to+1)*4000000 + i
-			break
-		}
-	}
+	p := findNonCoveredPoint()
+	solution.Part2 = p.x*4000000 + p.y
 	return
 }
